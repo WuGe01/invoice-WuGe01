259da387c4ed15a09baf958e8019984ca32c3469
吳宗融 <bc781208@hotmail.com>
2020-05-12 16:34:44 +0800

    兌獎變數設定&測試OK&準備加入判斷式

--- award.php
+++ award.php
@@ -1,4 +1,48 @@
-<!DOCTYPE html>
+<?php
+include "./layout/pdo.php";
+session_start();
+$years=$_POST['years'];
+$period=$_POST['period'];
+$sql1="select `id`, `code`, `number` from `invoices` where `period` = '$period' AND `years` = '$years' ";
+// echo  $sql1 . "<br>";
+$sql2="select * from `award` where `period` = '$period' AND `years` = '$years' ";
+// echo  $sql2;
+$row1=$pdo->query($sql1)->fetchAll(PDO::FETCH_ASSOC);
+$row2=$pdo->query($sql2)->fetch(PDO::FETCH_ASSOC);
+echo  "<pre>";
+echo print_r($row1);
+echo  "</pre>";
+echo  "<pre>";
+echo print_r($row2);
+echo  "</pre>";
+$Special_award=strrev($row2['Special_award']);
+$S_award=strrev($row2['S_award']);
+$h1_award=strrev($row2['h1_award']);
+$h2_award=strrev($row2['h2_award']);
+$h3_award=strrev($row2['h3_award']);
+$six_award=strrev($row2['six_award']);
+$six2_award=strrev($row2['six2_award']);
+$six3_award=strrev($row2['six3_award']);
+echo $Special_award."<br>";
+echo $S_award."<br>";
+echo $h1_award."<br>";
+echo $h2_award."<br>";
+echo $h3_award."<br>";
+echo $six_award."<br>";
+echo $six2_award."<br>";
+echo $six3_award."<br>";
+echo substr($Special_award, 0,3);
+
+foreach($row1 as $r){
+
+
+
+}
+
+
+
+?>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,4 +54,4 @@
 <body>
     <?php include "./layout/header.php" ;?>
 </body>
-</html>
+</html> -->
