fac99c4eb33ade45ca75bebe2eedc1bf9a491489
吳宗融 <bc781208@hotmail.com>
2020-05-13 10:20:07 +0800

    對發票邏輯完成&缺CSS版面設計

--- award.php
+++ award.php
@@ -4,17 +4,12 @@ session_start();
 $years=$_POST['years'];
 $period=$_POST['period'];
 $sql1="select `id`, `code`, `number` from `invoices` where `period` = '$period' AND `years` = '$years' ";
-// echo  $sql1 . "<br>";
+
 $sql2="select * from `award` where `period` = '$period' AND `years` = '$years' ";
-// echo  $sql2;
+
 $row1=$pdo->query($sql1)->fetchAll(PDO::FETCH_ASSOC);
 $row2=$pdo->query($sql2)->fetch(PDO::FETCH_ASSOC);
-echo  "<pre>";
-echo print_r($row1);
-echo  "</pre>";
-echo  "<pre>";
-echo print_r($row2);
-echo  "</pre>";
+
 $Special_award=strrev($row2['Special_award']);
 $S_award=strrev($row2['S_award']);
 $h1_award=strrev($row2['h1_award']);
@@ -23,21 +18,82 @@ $h3_award=strrev($row2['h3_award']);
 $six_award=strrev($row2['six_award']);
 $six2_award=strrev($row2['six2_award']);
 $six3_award=strrev($row2['six3_award']);
-echo $Special_award."<br>";
-echo $S_award."<br>";
-echo $h1_award."<br>";
-echo $h2_award."<br>";
-echo $h3_award."<br>";
-echo $six_award."<br>";
-echo $six2_award."<br>";
-echo $six3_award."<br>";
-echo substr($Special_award, 0,3);
 
 foreach($row1 as $r){
+// echo $r['number']."<br>"; 
+$number=strrev($r['number']);
 
+if($number == $Special_award){
+    echo $r['code'] . "-" . $r['number'];
+    echo "特別獎獎金1,000萬元<br>";
+}else if($number == $S_award){
+    echo $r['code'] . "-" . $r['number'];
+    echo "特獎獎金200萬元<br>";
 
+    // 頭獎1
+}else if(substr($number, 0,7) == substr($h1_award, 0,7)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金4萬元<br>";
+}else if(substr($number, 0,6) == substr($h1_award, 0,6)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金1萬元<br>";
+}else if(substr($number, 0,5) == substr($h1_award, 0,5)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金4千元<br>";
+}else if(substr($number, 0,4) == substr($h1_award, 0,4)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金1千元<br>";
+}else if(substr($number, 0,3) == substr($h1_award, 0,3)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金2百元<br>";
 
+    // 頭獎2
+}else if(substr($number, 0,7) == substr($h2_award, 0,7)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金4萬元<br>";
+}else if(substr($number, 0,6) == substr($h2_award, 0,6)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金1萬元<br>";
+}else if(substr($number, 0,5) == substr($h2_award, 0,5)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金4千元<br>";
+}else if(substr($number, 0,4) == substr($h2_award, 0,4)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金1千元<br>";
+}else if(substr($number, 0,3) == substr($h2_award, 0,3)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金2百元<br>";
+
+    // 頭獎3
+}else if(substr($number, 0,7) == substr($h3_award, 0,7)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金4萬元<br>";
+}else if(substr($number, 0,6) == substr($h3_award, 0,6)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金1萬元<br>";
+}else if(substr($number, 0,5) == substr($h3_award, 0,5)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金4千元<br>";
+}else if(substr($number, 0,4) == substr($h3_award, 0,4)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金1千元<br>";
+}else if(substr($number, 0,3) == substr($h3_award, 0,3)){
+    echo $r['code'] . "-" . $r['number'];
+    echo "獎金2百元<br>";
+
+    // 六獎
+}else if(substr($number, 0,3) == $six_award){
+    echo $r['code'] . "-" . $r['number'];
+    echo "六獎獎金2百元<br>";  
+}else if(substr($number, 0,3) == $six2_award){
+    echo $r['code'] . "-" . $r['number'];
+    echo "六獎獎金2百元<br>";  
+}else if(substr($number, 0,3) == $six3_award){
+    echo $r['code'] . "-" . $r['number'];
+    echo "六獎獎金2百元<br>";  
 }
+
+};
 
 
 
