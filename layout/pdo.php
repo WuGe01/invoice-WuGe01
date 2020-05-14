0b2ae7ccf6d7eb5824332a77a1a75950ef8a4b12
吳宗融 <bc781208@hotmail.com>
2020-05-14 09:57:40 +0800

    初版完成-V1.0.0版

--- layout/pdo.php
+++ layout/pdo.php
@@ -1,7 +1,5 @@
 <?php
 $dns="mysql:host=localhost;charset=utf8;dbname=invoice";
 $pdo=new PDO($dns,"root","");
-// $dns="mysql:host=localhost;charset=utf8;dbname=s1090202";
-// $pdo=new PDO($dns,"s1090202","s1090202");
 date_default_timezone_set("Asia/Taipei");
 ?>
